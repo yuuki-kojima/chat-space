75c5d726e944b0ffa4a2acc507a3e307d2a17256
Yuuki Kojima <yuuki.xx54@gmail.com>
2018-03-11 19:12:25 +0900

    ルーティングの作成
    ルーティングを作成し、各コントローラーにアクションの入れ物を作成した。

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,8 @@
 Rails.application.routes.draw do
-  resource :users, only: [:edit, :update]
+  resource :users, only: [:index, :edit, :update]
   devise_for :users
   root 'messages#index'
+  resource :groups, only: [:new, :create, :edit, :update] do
+    resource :messages, only: [:index, :create]
+  end
 end
