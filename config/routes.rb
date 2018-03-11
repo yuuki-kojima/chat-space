e4ac179fe5dfc33d39f9627bcbc10121e46abbc4
Yuuki Kojima <yuuki.xx54@gmail.com>
2018-03-11 21:15:16 +0900

    グループ機能の実装

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,8 @@
 Rails.application.routes.draw do
-  resource :users, only: [:index, :edit, :update]
   devise_for :users
-  root 'messages#index'
-  resource :groups, only: [:new, :create, :edit, :update] do
-    resource :messages, only: [:index, :create]
+  root 'groups#index'
+  resources :users, only: [:index, :edit, :update]
+  resources :groups, only: [:new, :create, :edit, :update] do
+    resources :messages, only: [:index, :create]
   end
 end
