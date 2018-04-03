b14820c17346e9609bd5c3bbdffa5bdbeba1f986
Yuuki Kojima <yuuki.xx54@gmail.com>
2018-04-03 19:19:48 +0900

    メッセージ送信機能の非同期通信の実装
    cssで高さが1px高かったので、合わせて修正しています。

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,54 @@
+$(document).on('turbolinks:load', function() {
+  function buildHTML(message){
+    if(message.image_url != null){
+      var imageHtml = `<img class="lower-message__image" src="${message.image_url}">`;
+    }else{
+      var imageHtml = '';
+    }
+    var html = `<div class="message">
+                  <div class="upper-message">
+                    <div class="upper-message__user-name">
+                    ${message.user_name}
+                    </div>
+                    <div class="upper-message__date">
+                    ${message.created_at}
+                    </div>
+                  </div>
+                  <div class="lower-meesage">
+                    <p class="lower-message__content">
+                    ${message.content}
+                    </p>
+                    ${imageHtml}
+                  </div>
+                </div>`
+    return html;
+  }
+  function autoScroll() {
+    var obj = $('.messages');
+    obj.get(0).scrollTop = obj.get(0).scrollHeight;
+  }
+  autoScroll();
+  $('#new_message').on('submit', function(e){
+    e.preventDefault();
+    var formData =  new FormData(this);
+    var url = $(this).attr('action')
+    $('.form__submit').removeAttr('data-disable-with');
+    $.ajax({
+      url: url,
+      type: "POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      $(html).appendTo('.messages');
+      $('.form__message').val('');
+      autoScroll();
+    })
+    .fail(function(){
+      alert('メッセージを送信できませんでした。');
+    })
+  })
+})
