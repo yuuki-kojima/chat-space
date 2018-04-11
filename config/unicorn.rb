d56b558f81cd50a735449296720184d79a91f445
Yuuki Kojima <yuuki.xx54@gmail.com>
2018-04-11 18:50:11 +0900

    capistranoの導入

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,11 +3,11 @@ app_path = File.expand_path('../../', __FILE__)
 worker_processes 1
 
 working_directory app_path
-pid "#{app_path}/tmp/pids/unicorn.pid"
-stderr_path "#{app_path}/log/unicorn.stderr.log"
-stdout_path "#{app_path}/log/unicorn.stdout.log"
+pid "#{app_path}/shared/tmp/pids/unicorn.pid"
+stderr_path "#{app_path}/shared/log/unicorn.stderr.log"
+stdout_path "#{app_path}/shared/log/unicorn.stdout.log"
 
-listen "#{app_path}/tmp/sockets/unicorn.sock"
+listen "#{app_path}/shared/tmp/sockets/unicorn.sock"
 timeout 60
 
 preload_app true
